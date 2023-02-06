17b0fb413f4ad7f1e9b54eb2f5a224f514e5f055
FalloutBoy379 <anshmmehta379@gmail.com>
2023-02-06 23:50:34 +0530

    Added class for future plans to store state of Robot after Autonomous

---
+++ Matrix_V2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TransferClass.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+
+public class TransferClass {
+    public static Pose2d poseStorage = null;
+    public static double turretAngle = 0;
+    public static double liftHeight = 0;
+}
